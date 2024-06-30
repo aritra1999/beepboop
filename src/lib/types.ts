e844868846de951a8a97014d5b7392a333ab2299
Aritra Mondal <aritra041999.dev@gmail.com>
2024-06-30 13:06:39 +0200

    feat: add necessary types

---
+++ src/lib/types.ts
@@ -0,0 +1,34 @@
+export interface Profile {
+	login: string;
+	id: number;
+	avatar_url: string;
+	name: string;
+	company: string;
+	blog: string;
+	location: string;
+	email: string | null;
+	hireable: boolean;
+	bio: string;
+	twitter_username: string;
+	public_repos: number;
+	public_gists: number;
+	followers: number;
+	following: number;
+	created_at: Date;
+	updated_at: Date;
+}
+
+export interface Content {
+	config: {
+		showInformation: boolean;
+		styles: string;
+	};
+	items: BentoItem[];
+}
+
+export interface BentoItem {
+	title: string;
+	description: string;
+	type: 'card-basic';
+	style: string;
+}
