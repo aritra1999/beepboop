65daf9c6c46a5b886305e3183fc604686a7b17bf
Aritra Mondal <aritra041999.dev@gmail.com>
2024-07-03 21:37:40 +0200

    feat: Add GitStats component to display GitHub statistics in user profile

---
+++ src/lib/components/ui/Profile/GitStats.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { Profile } from '$lib/types';
+
+	export let profile: Profile;
+</script>
+
+<div class="grid grid-cols-2 grid-rows-2">
+	<div class="stat-base">
+		<div class="text-xl font-semibold">{profile.public_repos}</div>
+		<div class="text-xs">Repos</div>
+	</div>
+	<div class="stat-base">
+		<div class="text-xl font-semibold">{profile.public_gists}</div>
+		<div class="text-xs">Gists</div>
+	</div>
+	<div class="stat-base">
+		<div class="text-xl font-semibold">{profile.followers}</div>
+		<div class="text-xs">Followers</div>
+	</div>
+	<div class="stat-base">
+		<div class="text-xl font-semibold">{profile.following}</div>
+		<div class="text-xs">Following</div>
+	</div>
+</div>
+
+<style>
+	.stat-base {
+		@apply flex h-24 flex-col items-center justify-center border-t-2 border-foreground odd:border-r-2;
+	}
+</style>
