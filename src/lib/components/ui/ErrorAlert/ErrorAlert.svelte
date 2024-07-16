736a62421d0976f72a600985204e141a69a3fb30
Aritra Mondal <aritra041999.dev@gmail.com>
2024-07-16 13:54:15 +0200

    feat: Add ErrorAlert component for displaying error messages

---
+++ src/lib/components/ui/ErrorAlert/ErrorAlert.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { CircleAlert } from 'lucide-svelte';
+	import * as Alert from '$lib/components/ui/alert';
+	import type { AlertError } from '$lib/types';
+
+	export let error: AlertError;
+</script>
+
+<div class="w-96">
+	<Alert.Root variant="destructive">
+		<CircleAlert class="h-4 w-4" />
+		<Alert.Title>{error.title}</Alert.Title>
+		{#each error.description as description}
+			<Alert.Description>{description}</Alert.Description>
+		{/each}
+	</Alert.Root>
+</div>
