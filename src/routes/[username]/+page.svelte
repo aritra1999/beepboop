db4d8c6715b9d4966bfef938db19e504022b1b90
Aritra Mondal <aritra041999.dev@gmail.com>
2024-07-16 13:54:20 +0200

    refactor: Improve profile page component

--- src/routes/[username]/+page.svelte
+++ src/routes/[username]/+page.svelte
@@ -1,20 +1,70 @@
-<script>
-	import BentoGrid from '$lib/components/ui/BentoGrid/BentoGrid.svelte';
-	import BentoGridItem from '$lib/components/ui/BentoGrid/BentoGridItem.svelte';
-	import ProfileCard from '$lib/components/ui/Profile/ProfileCard.svelte';
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { sleep } from '$lib/utils';
+	import { page } from '$app/stores';
+	import type { AlertError, Content, Profile } from '$lib/types';
+	import { dummyContent, dummyProfile } from '$lib/dummy';
+	import { UserNameSchema, validate } from '$lib/zod.types';
+	import type { ZodError } from 'zod';
 
-	export let data;
+	import BentoGrid from '$lib/components/ui/BentoGrid/BentoGrid.svelte';
+	import ProfileCard from '$lib/components/ui/Profile/ProfileCard.svelte';
+	import ErrorAlert from '$lib/components/ui/ErrorAlert/ErrorAlert.svelte';
+	import BentoGridItem from '$lib/components/ui/BentoGrid/BentoGridItem.svelte';
+	import LoadingProfile from '$lib/components/ui/LoadingProfile/Loading.svelte';
+
+	let loading = true;
+	let error: AlertError;
+	let profile: Profile;
+	let content: Content;
+
+	const loadGithubProfile = async (username: string): Promise<Profile> => {
+		console.log(`Fetching github profile for ${username}`);
+		return dummyProfile;
+	};
+
+	const loadBentoContent = async (username: string) => {
+		console.log(`Fetching bento content for ${username}`);
+		return dummyContent;
+	};
+
+	onMount(async () => {
+		loading = true;
+		const username = $page.params.username;
+		const validation = validate(username, UserNameSchema);
+		if (!validation.success) {
+			const zodErrors = await validation.error.message;
+			error = {
+				title: 'Invalid username',
+				description: JSON.parse(zodErrors).map((error: ZodError) => error.message)
+			};
+			loading = false;
+			return;
+		}
+
+		await sleep(2000);
+		profile = await loadGithubProfile(username);
+		content = await loadBentoContent(username);
+
+		loading = false;
+	});
 </script>
 
-{#if data.profile && data.content}
+{#if !loading}
 	<section class="flex h-screen items-center justify-center">
-		<BentoGrid className="mx-auto overflow-y-auto sm:h-[65vh] h-[90vh] px-3">
-			<ProfileCard profile={data.profile} showGithubStats={data.content.config.showGithubStats} />
-			{#each data.content.items as item, i (i)}
-				<BentoGridItem {item} />
-			{/each}
-		</BentoGrid>
+		{#if error}
+			<ErrorAlert {error} />
+		{:else if profile && content}
+			<BentoGrid className="mx-auto overflow-y-auto sm:h-[65vh] h-[90vh] px-3">
+				<ProfileCard {profile} showGithubStats={content.config.showGithubStats} />
+				{#each content.items as item, i (i)}
+					<BentoGridItem {item} />
+				{/each}
+			</BentoGrid>
+		{:else}
+			No profile found :(
+		{/if}
 	</section>
 {:else}
-	No profile found :(
+	<LoadingProfile />
 {/if}
