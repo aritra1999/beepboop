b92a377e31917d5d6953626a29fc4303629ed263
Aritra Mondal <aritra041999.dev@gmail.com>
2024-07-16 13:54:08 +0200

    feat: Add LoadingProfile component for displaying a loading animation during profile loading

---
+++ src/lib/components/ui/LoadingProfile/Loading.svelte
@@ -0,0 +1,9 @@
+<div
+	class="absolute left-0 top-0 flex h-screen w-screen items-center justify-center bg-[#16151A] text-background"
+>
+	<img
+		src="https://media.giphy.com/media/xTkcEQACH24SMPxIQg/giphy.gif?cid=ecf05e47dyo08a4tz0a5o7zt4uwerticewzjaj5l4wisdz72&ep=v1_gifs_search&rid=giphy.gif&ct=g"
+		alt="Loading..."
+		class="w-[50vh]"
+	/>
+</div>
