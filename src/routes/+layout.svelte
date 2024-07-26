1423756dfe975373d2669479ea2bb8ccf07947c5
Aritra Mondal <aritra041999.dev@gmail.com>
2024-07-26 23:33:06 +0200

    feat: Add navbar component to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 	import '../fonts.css';
 	import '../app.css';
+	import Navbar from '$lib/components/ui/Navbar/navbar.svelte';
 </script>
 
 <main class="h-screen w-screen overflow-y-auto">
-	<div class="container mx-auto">
+	<div class="container mx-auto px-10">
+		<Navbar />
 		<slot />
 	</div>
 </main>
