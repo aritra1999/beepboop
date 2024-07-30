893333ee3c3e8101f64a9efb57c8a621153a6ecb
Aritra Mondal <aritra041999.dev@gmail.com>
2024-07-30 09:41:59 +0200

    refactor: Update LinkCard titles and descriptions

--- src/routes/docs/components/+page.svelte
+++ src/routes/docs/components/+page.svelte
@@ -20,7 +20,7 @@
 			}
 		},
 		{
-			title: 'link with text',
+			title: 'link card with text',
 			description: 'The link card component with text',
 			component: LinkCard,
 			input: {
@@ -31,40 +31,45 @@
 			}
 		},
 		{
-			title: 'link with icon',
+			title: 'link card with icon',
 			description: 'The link card component with icon',
 			component: LinkCard,
 			input: {
 				type: 'link',
 				title: 'LinkedIn',
-				style: 'h-full',
+				style: 'h-full bg-blue-800 text-white',
 				link: 'https://linkedin.com',
 				linkIcon:
 					'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>'
 			}
 		},
 		{
-			title: 'link with background image',
+			title: 'link card with background image',
 			description: 'The link card component with icon',
 			component: LinkCard,
 			input: {
 				type: 'link',
 				title: 'LinkedIn',
-				style: 'h-full',
+				style: 'h-full text-white',
+				link: 'https://linkedin.com',
+				backgroundImage:
+					'https://media.istockphoto.com/id/1392898737/vector/abstract-horizontal-background-with-colorful-waves.jpg?s=612x612&w=0&k=20&c=xxRagBOWElqfzxPho893QoNmB1HnAw9VM-UHkQhtih4=',
+				linkIcon:
+					'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>'
+			}
+		},
+
+		{
+			title: 'link card with background image',
+			description: 'The link card component with icon',
+			component: LinkCard,
+			input: {
+				type: 'link',
+				title: 'LinkedIn',
+				style: 'h-full text-white',
 				link: 'https://linkedin.com',
 				backgroundImage:
 					'https://media.istockphoto.com/id/1392898737/vector/abstract-horizontal-background-with-colorful-waves.jpg?s=612x612&w=0&k=20&c=xxRagBOWElqfzxPho893QoNmB1HnAw9VM-UHkQhtih4='
-			}
-		},
-		{
-			title: 'default',
-			description: 'The default card component',
-			component: Default,
-			input: {
-				type: 'default',
-				style: 'p-4 h-full',
-				title: 'Title',
-				description: 'Description'
 			}
 		},
 		{
