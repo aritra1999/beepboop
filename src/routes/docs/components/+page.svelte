ada63eaa09a0a348344debd839d54981c2123fe1
Aritra Mondal <aritra041999.dev@gmail.com>
2024-07-30 09:39:35 +0200

    feat: replace placeholder cards in components page

--- src/routes/docs/components/+page.svelte
+++ src/routes/docs/components/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-	import Default from '$lib/components/ui/BentoGrid/Card/Default.svelte';
 	import { Button } from '$lib/components/ui/button';
 	import { Copy } from 'lucide-svelte';
 	import type { PageData } from './$types';
+	import Default from '$lib/components/ui/BentoGrid/Card/Default.svelte';
+	import LinkCard from '$lib/components/ui/BentoGrid/Card/LinkCard.svelte';
 
 	export let data: PageData;
 
@@ -19,25 +20,40 @@
 			}
 		},
 		{
-			title: 'default',
-			description: 'The default card component',
-			component: Default,
+			title: 'link with text',
+			description: 'The link card component with text',
+			component: LinkCard,
 			input: {
-				type: 'default',
-				style: 'p-4 h-full',
-				title: 'Title',
-				description: 'Description'
+				type: 'link',
+				title: 'LinkedIn',
+				style: 'h-full bg-blue-800 text-white',
+				link: 'https://linkedin.com'
 			}
 		},
 		{
-			title: 'default',
-			description: 'The default card component',
-			component: Default,
+			title: 'link with icon',
+			description: 'The link card component with icon',
+			component: LinkCard,
 			input: {
-				type: 'default',
-				style: 'p-4 h-full',
-				title: 'Title',
-				description: 'Description'
+				type: 'link',
+				title: 'LinkedIn',
+				style: 'h-full',
+				link: 'https://linkedin.com',
+				linkIcon:
+					'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>'
+			}
+		},
+		{
+			title: 'link with background image',
+			description: 'The link card component with icon',
+			component: LinkCard,
+			input: {
+				type: 'link',
+				title: 'LinkedIn',
+				style: 'h-full',
+				link: 'https://linkedin.com',
+				backgroundImage:
+					'https://media.istockphoto.com/id/1392898737/vector/abstract-horizontal-background-with-colorful-waves.jpg?s=612x612&w=0&k=20&c=xxRagBOWElqfzxPho893QoNmB1HnAw9VM-UHkQhtih4='
 			}
 		},
 		{
